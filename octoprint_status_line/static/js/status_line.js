504c6a9233bad0efdae69ab479bcff8290759581
palindrom <8235214+LMS0815@users.noreply.github.com>
2020-05-08 19:19:05 +0200

    First release

---
+++ octoprint_status_line/static/js/status_line.js
@@ -0,0 +1,37 @@
+$(function() {
+    function StatusLineViewModel() {
+        var self = this;
+
+        self.status_line = ko.observable();
+        self.show_status = ko.observable(false);
+
+        self.initialMessage = function(data) {
+            self.status_line(data.status_line);
+            self.show_status(data.status_line ? true : false);
+        };
+
+        self.onStartupComplete = function() {
+            // WebApp started, get message if any
+            $.ajax({
+                url: API_BASEURL + "plugin/pctoprint_status_line",
+                type: "GET",
+                dataType: "json",
+                success: self.initialMessage
+            });
+        };
+
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "status_line") {
+                return;
+            }
+            self.status_line((data.status_line+'\n'+(self.status_line()?self.status_line():'')).substring(0, 300));
+            self.show_status(true);
+        };
+    }
+
+    OCTOPRINT_VIEWMODELS.push([
+        StatusLineViewModel,
+        [ ],
+        ["#status_line"]
+    ]);
+});
